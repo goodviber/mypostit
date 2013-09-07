438264b6c54049b21ce69f8c35f96d13da915875
Jason Robinett <jrobinettwa@gmail.com>
2013-09-07 13:11:05 -0700

    adding new relationships

---
+++ db/migrate/20130907190959_create_postcategory.rb
@@ -0,0 +1,10 @@
+class CreatePostcategory < ActiveRecord::Migration
+  def change
+    create_table :postcategories do |t|
+    	t.integer :post_id
+    	t.integer :category_id
+
+    	t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907012243) do
+ActiveRecord::Schema.define(version: 20130907195102) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "comments"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "post_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "url"
@@ -19,6 +40,7 @@ ActiveRecord::Schema.define(version: 20130907012243) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
